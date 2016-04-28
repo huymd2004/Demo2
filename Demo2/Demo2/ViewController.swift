b324f80be4f444c26d4d3ae98d766dcf877b6a41
huymd <v.huymd@vincommerce.com>
2016-04-28 16:42:07 +0700

    add code

---
+++ Demo2/Demo2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo2
+//
+//  Created by Mac Dang Huy on 4/28/16.
+//  Copyright © 2016 Mac Dang Huy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
